ea8122e2fc90433b6dd1e09ef22ce2c2cf2f47ed
makattack <makaylaroserussell@gmail.com>
2023-10-05 19:17:25 -0700

    Added API keys

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const bingAPI = "Au5J2-W-wm9RjOdzUylDQNJaiPua_uIVGKvz3mkQfCVvnTBPmZsuS3C8RztArOLq";
+const spoitfyAPI = "a11f5e96a3ce48faba28d169a9120f77";
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
