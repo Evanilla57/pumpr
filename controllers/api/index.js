0e04cc444f256bb9c41be88c1d15210612896713
Evanilla57 <grigg005@cougars.csusm.edu>
2023-10-10 19:19:27 -0700

    added const profileRoutes to api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
+const profileRoutes = require('./profileRoutes');
 const bingRoutes = require('./bingRoutes');
 
 router.get(
